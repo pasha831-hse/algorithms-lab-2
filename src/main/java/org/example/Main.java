4da7828046047f87b4022442e1df8c1502c501cc
pasha <pashamedvedev03@gmail.com>
2023-04-20 17:35:20 +0300

    implement and test brute force solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,52 @@
 package org.example;
 
+import org.example.model.Point;
+import org.example.model.Rectangle;
+import org.example.solution.BruteForceSolution;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static String sendGreetings() {
-        return "Hello world!";
-    }
     public static void main(String[] args) {
-        System.out.println(sendGreetings());
+        // Get rectangles from stdin and save them inside rectangles list
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfRectangles = scanner.nextInt();
+        List<Rectangle> rectangles = new ArrayList<>();
+
+        for (int i = 0; i < numberOfRectangles; i++) {
+            int x1 = scanner.nextInt();
+            int y1 = scanner.nextInt();
+            int x2 = scanner.nextInt();
+            int y2 = scanner.nextInt();
+
+            rectangles.add(new Rectangle(x1, y1, x2, y2));
+        }
+
+        // Create result list and
+        // get points from stdin and save them inside points list
+        List<Point> points = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+
+        int numberOfPoints = scanner.nextInt();
+        for (int i = 0; i < numberOfPoints; i++) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+
+            Point point = new Point(x, y);
+            points.add(point);
+        }
+
+        // Create brute force solution class
+        // and save answers inside result array
+        BruteForceSolution solution = new BruteForceSolution(rectangles);
+        for (Point point : points) {
+            result.add(solution.solvePoint(point));
+        }
+
+        System.out.println(Arrays.toString(result.toArray()).replace("[", "").replace("]", "").replace(",", ""));
     }
 }
