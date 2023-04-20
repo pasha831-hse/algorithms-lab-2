a884abe47b7921f356b67b1dd4fa9cbaf3f271dd
pasha <pashamedvedev03@gmail.com>
2023-04-20 17:32:16 +0300

    add models: Point and Rectangle

---
+++ src/main/java/org/example/model/Rectangle.java
@@ -0,0 +1,13 @@
+package org.example.model;
+
+import org.example.model.Point;
+
+public class Rectangle {
+    public Point leftDownPoint;
+    public Point rightUpPoint;
+
+    public Rectangle(int x1, int y1, int x2, int y2) {
+        this.leftDownPoint = new Point(x1, y1);
+        this.rightUpPoint = new Point(x2, y2);
+    }
+}
