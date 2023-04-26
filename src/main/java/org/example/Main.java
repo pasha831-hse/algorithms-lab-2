61cf6f25b60c8e2bde322f5e29c023e4462bf23f
pasha <pashamedvedev03@gmail.com>
2023-04-26 15:11:00 +0300

    implement all solutions testing, rewrite main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,56 +1,16 @@
 package org.example;
 
-import org.example.model.Point;
-import org.example.model.Rectangle;
+import org.example.benchmarking.Benchmark;
 import org.example.solution.BruteForceSolution;
 import org.example.solution.CompressedMapSolution;
 import org.example.solution.LPSTSolution;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        // Get rectangles from stdin and save them inside rectangles list
-        Scanner scanner = new Scanner(System.in);
-
-        int numberOfRectangles = scanner.nextInt();
-        List<Rectangle> rectangles = new ArrayList<>();
-
-        for (int i = 0; i < numberOfRectangles; i++) {
-            int x1 = scanner.nextInt();
-            int y1 = scanner.nextInt();
-            int x2 = scanner.nextInt();
-            int y2 = scanner.nextInt();
-
-            rectangles.add(new Rectangle(x1, y1, x2, y2));
-        }
-
-        // Create result list and
-        // get points from stdin and save them inside points list
-        List<Point> points = new ArrayList<>();
-        List<Integer> result = new ArrayList<>();
-
-        int numberOfPoints = scanner.nextInt();
-        for (int i = 0; i < numberOfPoints; i++) {
-            int x = scanner.nextInt();
-            int y = scanner.nextInt();
-
-            Point point = new Point(x, y);
-            points.add(point);
-        }
-
-        // Create solution class
-        // and save answers inside result array
-//        BruteForceSolution solution = new BruteForceSolution(rectangles);
-//        CompressedMapSolution solution = new CompressedMapSolution(rectangles);
-        LPSTSolution solution = new LPSTSolution(rectangles);
-        for (Point point : points) {
-            result.add(solution.solvePoint(point));
-        }
-
-        System.out.println(Arrays.toString(result.toArray()).replace("[", "").replace("]", "").replace(",", ""));
+        Benchmark.runAllTests(
+                new BruteForceSolution(),
+                new CompressedMapSolution(),
+                new LPSTSolution()
+        );
     }
 }
--- src/main/java/org/example/benchmarking/Benchmark.java
+++ src/main/java/org/example/benchmarking/Benchmark.java
@@ -6,9 +6,23 @@ import org.example.solution.BruteForceSolution;
 import org.example.solution.CompressedMapSolution;
 import org.example.solution.LPSTSolution;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class Benchmark {
+    public static PrintWriter writer;
+    public static String filePath = "./artefacts/measurement.txt";
+
+    static {
+        try {
+            writer = new PrintWriter(filePath, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static long benchmarkPreparation(
             BruteForceSolution bruteForceSolution,
             List<Rectangle> rectangles
@@ -88,5 +102,53 @@ public class Benchmark {
         long endBenchmark = System.nanoTime();
 
         return endBenchmark - startBenchmark;
+    }
+
+    public static void testSolutions(
+            BruteForceSolution bruteForceSolution,
+            CompressedMapSolution compressedMapSolution,
+            LPSTSolution lpstSolution,
+            int numberOfRandP
+    ) {
+        List<Rectangle> rectangles = RectPointGenerator.generateRecommendedRectangles(numberOfRandP);
+        List<Point> points = RectPointGenerator.generateRecommendedPoints(numberOfRandP);
+
+        long timeToPrepareBruteForce = Benchmark.benchmarkPreparation(bruteForceSolution, rectangles);
+        long timeToPrepareCompressedMap = Benchmark.benchmarkPreparation(compressedMapSolution, rectangles);
+        long timeToPrepareLPST = Benchmark.benchmarkPreparation(lpstSolution, rectangles);
+
+        writer.println("PrepareN:" + numberOfRandP);
+        writer.println("BruteForce:" + timeToPrepareBruteForce);
+        writer.println("CompressedMap:" + timeToPrepareCompressedMap);
+        writer.println("LPST:" + timeToPrepareLPST);
+        writer.println();
+
+        long bruteForceDuration = Benchmark.benchmark(bruteForceSolution, rectangles, points);
+        long compressedMapDuration = Benchmark.benchmark(compressedMapSolution, rectangles, points);
+        long LPSTDuration = Benchmark.benchmark(lpstSolution, rectangles, points);
+
+        writer.println("RunN:" + numberOfRandP);
+        writer.println("BruteForce:" + bruteForceDuration);
+        writer.println("CompressedMap:" + compressedMapDuration);
+        writer.println("LPST:" + LPSTDuration);
+        writer.println();
+    }
+
+    public static void runAllTests(
+            BruteForceSolution bruteForceSolution,
+            CompressedMapSolution compressedMapSolution,
+            LPSTSolution lpstSolution
+    ) {
+        int[] numberOfRandP = new int[] {
+                10, 20, 50, 80, 100, 150, 250, 300, 400, 450,
+                500, 600, 700, 800, 900, 1000, 1300, 1500, 2000
+        };
+
+        for (int number : numberOfRandP) {
+            testSolutions(bruteForceSolution, compressedMapSolution, lpstSolution, number);
+            System.gc();
+        }
+
+        writer.close();
     }
 }
